903eb383d3b2695b45fe2bb2c37bf18f23dc9eb6
Роман <roman.stepanenko@mail.ru>
2024-07-02 09:49:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+Number_of_completed_homework="12"
+Number_of_hours_spent="1.5"
+Course_name="Python"
+Время_на_одно_задание=(1.5/12)
+print("Курс:",Course_name,", всего задач:",Number_of_completed_homework,", среднее время выполнения", Время_на_одно_задание, "часа")
+
